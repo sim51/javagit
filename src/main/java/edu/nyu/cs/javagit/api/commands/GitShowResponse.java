9b45b5af1c28e03bf71a3246edccf95002b9bac0
Benoît SIMARD <github@bsimard.com>
2011-08-08 20:58:13 +0200

    begin of "git show"

---
+++ src/main/java/edu/nyu/cs/javagit/api/commands/GitShowResponse.java
@@ -0,0 +1,10 @@
+package edu.nyu.cs.javagit.api.commands;
+
+public class GitShowResponse implements CommandResponse {
+
+    /**
+     * List of files that have been modified but not committed.
+     */
+    protected final Ref ref;
+
+}
